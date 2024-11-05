3b53223af8eac98d7cb70d556704b88f2593686f
Cwoobin <wohyuck@naver.com>
2024-11-05 11:15:57 +0900

    WHERE(조건)BETWEEN,IN,NULL테스팅

--- oracleDate/로컬-HR.sql
+++ oracleDate/로컬-HR.sql
@@ -23,3 +23,24 @@ select first_name, job_id from employees;
 select first_name || '의 직급은' || job_id ||'입니다' as data from employees;
 -- 중복되지 않게 보여주기 distinct
 select distinct job_id from employees;
+--연봉을 3000 이상 받는 사람의 정보
+select * from employees where salary >= 3000;
+desc employees;
+-- 2008년 이후에 입사한 직원 조사
+select * from employees where hire_date >= '2008/01/01';
+select * from employees where TO_CHAR(hire_date,'YYYY/MM/DD') >= '2008/01/01';
+select * from employees where hire_date >= TO_DATE('2008/01/01','YYYY/MM/DD HH24:MI:ss');
+-- AND , BETWEEN a AND B
+SELECT * FROM EMPLOYEES WHERE SALARY >= 2000 AND SALARY <= 3000;
+SELECT * FROM EMPLOYEES WHERE SALARY BETWEEN 2000 AND 3000;
+-- OR,IN( , )직원번호가 67이거나 101이거나 184인 사원
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID = 67 OR EMPLOYEE_ID = 101 OR EMPLOYEE_ID = 184; 
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID IN(67,101,184);
+-- NULL 연산, 비교, 할당이 안된다.
+SELECT 100 + NULL FROM DUAL;
+SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID IN(67,101,184);
+desc employees;
+SELECT * FROM EMPLOYEES WHERE COMMISSION_PCT IS NULL;
+SELECT * FROM EMPLOYEES WHERE COMMISSION_PCT IS NOT NULL;
+SELECT * FROM EMPLOYEES;
+
